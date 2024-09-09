88c7d474994870042f89163d1b05c0df0b7e32ed
Michel Shpilevsky <68427995+Nicheye@users.noreply.github.com>
2024-09-09 18:44:43 +0300

    final commit #1

---
+++ backend/backendmusic/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from backendmusic import settings
+from backendmusic.swagger import schema_view
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include("authentification.urls")),
+    path('api/', include("main.urls")),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
